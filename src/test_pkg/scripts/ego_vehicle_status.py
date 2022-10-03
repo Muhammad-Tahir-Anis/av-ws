db22b83c282ef8f070a37de03c7b31fb07993d47
mta <muhammadtahiranis@gmail.com>
2022-10-03 19:05:51 +0500

    v0.0.1

--- src/test_pkg/scripts/ego_vehicle_status.py
+++ src/test_pkg/scripts/ego_vehicle_status.py
@@ -1,6 +1,5 @@
 import math
 from math import cos, sin
-
 import rospy
 from carla_msgs.msg import CarlaEgoVehicleStatus
 from ego_vehicle_control import EgoController
@@ -9,9 +8,27 @@ from src.map_parser_pkg.scripts.odr_map_obj import opendrive
 
 
 class EgoVehicle:
+    # route = ["3", "0", "10", "17", "7", "90", "6", "735", "5", "516", "4", "8", "1", "675", "2", "566", "3"]
+    route = ["3", "0", "10"]
+    index = 0
+    s = 0
+    heading = 0
+    curvature = 0
 
     @classmethod
-    def xy_to_st(cls, x, y, road_id, heading):
+    def curvature_points(cls, curvature, curvature_length):
+        curvature_radius = 1 / curvature
+        curvature_angle = float(curvature_length) / curvature_radius
+        one_degree = math.radians(0.99981135250576669282)
+        s_list = []
+        for x in range(round(math.degrees(curvature_angle))):
+            if (x * one_degree) <= curvature_angle:
+                s_list.append((x * one_degree)*curvature_radius)
+        for s in s_list:
+            print(s)
+
+    @classmethod
+    def xy_to_st(cls, x, y, road_id):
         for road in opendrive.road_list:
             if road_id == road.id:
                 if road.planview.geometry_list:
@@ -20,62 +37,60 @@ class EgoVehicle:
                 else:
                     s_translation = (x - float(road.planview.geometry.x))
                     t_translation = (y - float(road.planview.geometry.y))
-                s_rotation = s_translation * cos(heading) + t_translation * sin(heading)
-                t_rotation = t_translation * cos(heading) - s_translation * sin(heading)
+                s_rotation = s_translation * cos(cls.heading) + t_translation * sin(cls.heading)
+                t_rotation = t_translation * cos(cls.heading) - s_translation * sin(cls.heading)
                 s = s_rotation
                 t = t_rotation
                 return s, t
 
-    # route = ["3", "0", "10", "17", "7", "90", "6", "735", "5", "516", "4", "8", "1", "675", "2", "566", "3"]
-    route = ["3", "0", "10"]
-    index = 0
-    s = 0
-    global heading, curvature
-
     @classmethod
     def update_route(cls, road_id, s):
+        global heading
         for road in opendrive.road_list:
             if road_id == road.id:
                 if road.planview.geometry_list:
                     for geometry in road.planview.geometry_list:
                         road_s = float(geometry.s)
-                        road_s = round(road_s,2)
-                        s = round(s,2)
-                        print(s, road_s)
-                        if s == road_s:
-                            cls.heading = float(geometry.hdg)
-                            if geometry.arc:
-                                cls.curvature = float(geometry.arc.curvature)
-                            else:
-                                cls.curvature = 0
+                        # road_s = round(road_s, 2)
+                        # s = round(s, 2)
+                        print("Road S: ", s, "Geometry S: ", road_s)
+                        print("Curvature : ", cls.curvature)
+                        if road.planview.geometry_list.index(geometry) < len(road.planview.geometry_list) - 1:
+                            if road_s <= s < float(
+                                    road.planview.geometry_list[road.planview.geometry_list.index(geometry) + 1].s):
+                                heading = float(geometry.hdg)
+                                if geometry.arc:
+                                    cls.curvature = float(geometry.arc.curvature)
+                                    cls.curvature_points(cls.curvature,road.length)
+                                else:
+                                    cls.curvature = 0
+                            # else:
+                            #     cls.curvature = 0
                 else:
-                    cls.heading = float(road.planview.geometry.hdg)
+                    heading = float(road.planview.geometry.hdg)
                     cls.curvature = 0
+                if cls.curvature != 0:
+                    radius_of_curvature = 1 / cls.curvature
+                    angle = cls.s / radius_of_curvature
+                    cls.heading = heading + angle
+                else:
+                    cls.heading = heading
                 road_length = float(road.length)
-                print(road_id, cls.heading, road_length, cls.curvature)
-                return road_id, cls.heading, road_length, cls.curvature
+                print("Road ID: ", road_id, "Road Length: ", road_length)
+                return road_id, road_length
 
     @classmethod
     def callback(cls, data):
-        # print(data)
-        global heading
         gnss = GnssData()
-        # route = iter(route)
         road = cls.route[cls.index]
-        road_id, heading, road_length, curvature = cls.update_route(road, cls.s)
-        if curvature != 0:
-            radius_of_curvature = 1 / curvature
-            angle = cls.s / radius_of_curvature
-            # angle = float(math.radians(angle))
-            print(angle)
-            heading = angle
-
-        print(heading)
-        cls.s, t = cls.xy_to_st(gnss.x, gnss.y, road_id, heading)
-        print(gnss.x, gnss.y)
-        print(cls.s, t, curvature)
+        road_id, road_length = cls.update_route(road, cls.s)
+        print("Road Heading: ", cls.heading)
+        cls.s, t = cls.xy_to_st(gnss.x, gnss.y, road_id)
+        print("X: ", gnss.x, "Y: ", gnss.y)
+        print("S: ", cls.s, "T: ", t, "Curvature: ", cls.curvature)
         if cls.s <= road_length:
-            EgoController(data.header, 0.15, -curvature*2.25, 0.0, 0, 0, 0, 0)
+            EgoController(data.header, 0.11, -cls.curvature * 2.25, 0.0, 0, 0, 0, 0)
+            # EgoController(data.header, 0.15, 0, 0.0, 0, 0, 0, 0)
         else:
             cls.index += 1
             cls.s = 0
@@ -84,7 +99,7 @@ class EgoVehicle:
 
     @classmethod
     def ego_vehicle_status_subscriber(cls):
-        sub = rospy.Subscriber('/carla/ego_vehicle/vehicle_status', CarlaEgoVehicleStatus, cls.callback)
+        rospy.Subscriber('/carla/ego_vehicle/vehicle_status', CarlaEgoVehicleStatus, cls.callback)
         rospy.spin()
 
 
