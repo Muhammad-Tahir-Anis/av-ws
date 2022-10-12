1c2a9035edb8ed30b85571a7bb833e05071a71b3
mta <muhammadtahiranis@gmail.com>
2022-10-12 19:05:37 +0500

    v0.0.1

--- src/test_pkg/scripts/run_ego_vehicle/axis_transformation.py
+++ src/test_pkg/scripts/run_ego_vehicle/axis_transformation.py
@@ -1,39 +1,82 @@
+import math
 from math import atan, sin, cos
+from logs import Log
 
 
 class AxisTransformation:
-    def __init__(self, x, y, x_origin, y_origin, heading, curvature, s_value):
+    def __init__(self, x, y, x_origin, y_origin, heading, curvature, s_value, log:Log):
         if curvature != 0:
             radius_of_curvature = 1 / curvature
-            print("Radius: ", radius_of_curvature)
+            # print("Radius: ", radius_of_curvature)
+            log.radius_of_curvature = radius_of_curvature
+
             # Translating Origin of axis to the starting point on road where s is initially 0
             self.x_prime, self.y_prime = self.__axis_translation(x, y, x_origin, y_origin)
-            print("Translation Curv: ", self.x_prime, self.y_prime)
+            # print("Translating to Curvature road: ", self.x_prime, self.y_prime)
+            log.translated_axis = [self.x_prime,self.y_prime]
+
             # Rotating axis along the direction of the road.
             self.x_double_prime, self.y_double_prime = self.__axis_rotation(self.x_prime, self.y_prime, heading)
-            print("Rotation Curv: ", self.x_double_prime, self.y_double_prime)
+            # print("Rotating up to Curvature road heading / angle: ", self.x_double_prime, self.y_double_prime)
+            log.rotated_axis = [self.x_double_prime, self.y_double_prime]
+
             # Translating axis to the center of curvature
-            self.adjacent, self.opposite = self.__axis_translation(self.x_double_prime, self.y_double_prime,
-                                                                   0, radius_of_curvature)
-            # To calculate original angle values we use
+            curvature_x_origin = 0
+            # When Curvature is positive
+            if curvature > 0:
+                curvature_y_origin = radius_of_curvature
+            # When Curvature is negative
+            else:
+                curvature_y_origin = -radius_of_curvature
+            self.x_curvature, self.y_curvature = self.__axis_translation(self.x_double_prime, self.y_double_prime,
+                                                                         curvature_x_origin, curvature_y_origin)
+
+            # print("Translating to Curvature Origin: ", self.x_curvature, self.y_curvature)
+            log.translated_axis_to_curvature_origin = [self.x_curvature,self.y_curvature]
+
+            # Again Rotating the axis as the starting of curvature becomes in First Quadrant
+            self.x_prime_curvature, self.y_prime_curvature = self.__axis_rotation(self.x_curvature, self.y_curvature,
+                                                                                  math.radians(-90))
+            # print("Rotating axis to normal: ", self.x_prime_curvature, self.y_prime_curvature)
+            log.rotated_axis_toward_curvature = [self.x_prime_curvature, self.y_prime_curvature]
+
+            # Define the adjacent and opposite of triangle to find angle and hypotenuses.
+            self.adjacent = self.y_prime_curvature
+            self.opposite = self.x_prime_curvature
+            # print("Adjacent & Opposite: ", self.adjacent, self.opposite)
+            log.adjacent = self.adjacent
+            log.opposite = self.opposite
+
+            # To calculate the angle values we use
             # tan(theta) = adjacent / opposite
             # theta = tan_inverse(adjacent / opposite)
-            print("Adjacent & Opposite: ", self.adjacent, self.opposite)
-            if self.opposite != 0:
-                angle_in_degree = atan(abs(self.adjacent) / abs(self.opposite))
-                print("Angle: ", angle_in_degree)
+            if self.opposite == 0:
+                angle_in_radian = 0
             else:
-                angle_in_degree = 0
+                angle_in_radian = atan(self.adjacent / self.opposite)
+                # print("Pre_angle: ", angle_in_radian)
+                log.angle_before_normalization = angle_in_radian
+
+                # Finding total angle
+                angle_in_radian = self.normalize_angle(self.adjacent, self.opposite, angle_in_radian)
+
+            # print("Angle: ", angle_in_radian)
+            log.angle_in_radian = angle_in_radian
+
             # To calculate S and T we use S = r*angle
             # For this purpose we use radius of curvature of the road.
-            self.s: float = abs(radius_of_curvature * angle_in_degree)+s_value
+            # print("Radius of curva: ", radius_of_curvature, " s_value: ", s_value)
+            log.radius_of_curvature = radius_of_curvature
+            self.s: float = abs(radius_of_curvature * angle_in_radian) + s_value
+
             # To get displacement of vehicle from origin which would be hypotenuse
-            # self.t: float = radius_of_curvature - math.sqrt(self.opposite ^ 2 + self.adjacent ^ 2)
-            self.t: float = 0
+            self.hypotenuse = math.sqrt(pow(self.opposite, 2) + pow(self.adjacent, 2))
+            self.t: float = radius_of_curvature - self.hypotenuse
+            # self.t: float = 0
         else:
             x_translated, y_translated = self.__axis_translation(x, y, x_origin, y_origin)
             self.s, self.t = self.__axis_rotation(x_translated, y_translated, heading)
-            self.s = self.s+s_value
+            self.s = self.s + s_value
 
     @classmethod
     def __axis_translation(cls, x, y, x_origin, y_origin):
@@ -42,11 +85,32 @@ class AxisTransformation:
         return x_translation, y_translation
 
     @classmethod
-    def __axis_rotation(cls, x_translated, y_translated, heading):
-        s = x_translated * cos(heading) + y_translated * sin(heading)
-        t = y_translated * cos(heading) - x_translated * sin(heading)
+    def __axis_rotation(cls, x, y, heading):
+        s = x * cos(heading) + y * sin(heading)
+        t = y * cos(heading) - x * sin(heading)
         # print(s, t)
         return s, t
+
+    @classmethod
+    def normalize_angle(cls, adjacent, opposite, angle_in_radian):
+        # First Quadrant
+        if opposite > 0 and adjacent >= 0:
+            pass
+
+        # Second Quadrant
+        elif opposite < 0 and adjacent >= 0:
+            # here in 2nd quadrant angle is in negative, so we will use + sign to subtract from 180
+            angle_in_radian = math.radians(180) + angle_in_radian
+
+        # Third Quadrant
+        elif opposite < 0 and adjacent < 0:
+            angle_in_radian = math.radians(180) + angle_in_radian
+
+        # Fourth Quadrant
+        elif opposite > 0 and adjacent < 0:
+            # here in 4th quadrant angle is in negative, so we will use + sign to subtract from 360
+            angle_in_radian = math.radians(360) + angle_in_radian
+        return angle_in_radian
 
     @property
     def s_t_axis(self):
--- src/test_pkg/scripts/run_ego_vehicle/trajectory.py
+++ src/test_pkg/scripts/run_ego_vehicle/trajectory.py
@@ -1,6 +1,7 @@
 from src.test_pkg.scripts.run_ego_vehicle.map_analysis import MapAnalysis
 
 from src.test_pkg.scripts.run_ego_vehicle.axis_transformation import AxisTransformation
+from logs import Log
 
 
 class Trajectory:
@@ -18,24 +19,32 @@ class Trajectory:
         self.brake: bool = False
         self.throttle: float = 0
         self.steering: float = 0
+        self.log = Log()
 
     def update_trajectory(self, x, y):
-        print("X: ", x, "Y: ", y)
-        print("S: ", self._s_axis, "T: ", self._t_axis)
-        print(self.path_index)
+        # print("X: ", x, "Y: ", y)
+        self.log.x = x
+        self.log.y = y
+        # print("S: ", self._s_axis, "T: ", self._t_axis)
+        self.log.s = self._s_axis
+        self.log.t = self._t_axis
+        # print(self.path_index)
         road_id = self.route[self.path_index]
         self.throttle, self.steering, self.brake = self.follow_trajectory(x, y, road_id)
-        print(self.throttle, self.steering, self.brake)
+        # print(self.throttle, self.steering, self.brake)
         return self.throttle, self.steering, self.brake
 
     def follow_trajectory(self, x, y, road_id):
         map_analysis = MapAnalysis()
-        x_origin, y_origin, heading, curvature, s_value, road_ended = map_analysis.road_info(road_id, self._s_axis)
-        print(x_origin, y_origin, heading, curvature, road_ended)
-        axis_transformation = AxisTransformation(x, y, x_origin, y_origin, heading, curvature, s_value)
+        x_origin, y_origin, heading, curvature, s_value, road_ended = map_analysis.road_info(road_id, self._s_axis,
+                                                                                             self.log)
+        # print(x_origin, y_origin, heading, curvature, road_ended)
+        self.log.heading = heading
+        axis_transformation = AxisTransformation(x, y, x_origin, y_origin, heading, curvature, s_value, self.log)
         self._s_axis, self._t_axis = axis_transformation.s_t_axis
         # print(self._s_axis, self._t_axis)
-        print("Path Index: ", self.path_index)
+        # print("Path Index: ", self.path_index)
+        self.log.path_index = self.path_index
         if road_ended:
             self._s_axis = 0
             self.path_index += 1
@@ -50,5 +59,6 @@ class Trajectory:
             self.throttle = 0.2
             self.brake = 0
             self.steering = 0
-        print(self.throttle, self.steering, self.brake)
+        self.log.set_log()
+        # print(self.throttle, self.steering, self.brake)
         return self.throttle, self.steering, self.brake
