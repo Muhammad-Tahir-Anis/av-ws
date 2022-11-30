7ca8d6ab6c8e82cd5581d6140422df19bfd1329e
mta <muhammadtahiranis@gmail.com>
2022-11-30 16:25:39 +0500

    v0.0.1

---
+++ src/test_pkg/scripts/object_detection/detect_obstacle.py
@@ -0,0 +1,43 @@
+from src.test_pkg.scripts.path_planning.Shortest_path_with_lanes import lane_list
+from src.test_pkg.scripts.path_planning.path_list_maker import path_list
+from src.test_pkg.scripts.path_planning.shortest_path_finder import shortest_path
+from src.test_pkg.scripts.run_ego_vehicle.ego_location import EgoLocation
+
+
+
+class ObjectLocation:
+
+    def __init__(self):
+        pass
+
+    @property
+    def get_path(self):
+        routes = path_list()
+        routes.make_list()
+        route_graph = shortest_path(routes.map_graph)
+
+        starting_road_id = "0"
+        end = "3"
+        path = route_graph.get_shortest_path_by_road_segments(starting_road_id, end)
+
+        start_lane = "-2"
+        lane_path = lane_list()
+        final_path_list = lane_path.lane_in_path(start_lane, path)
+        return final_path_list
+
+    @classmethod
+    def get_Obstacle_location(cls, x, y):
+        path = ObjectLocation()
+        vehicle_path = path.get_path
+        location = EgoLocation(x, y)
+        print(location.road_id)
+        print(vehicle_path)
+        for road in vehicle_path:
+            if road[0] == location.road_id:
+                print(road[0], location.road_id)
+                if int(road[1]) == int(location.lane_id):
+                    print(road[1], location.lane_id)
+
+
+
+
---
+++ src/test_pkg/scripts/object_detection/obstacle_gnss_data.py
@@ -0,0 +1,39 @@
+import rospy
+from sensor_msgs.msg import NavSatFix
+from src.test_pkg.scripts.carla_spawn_sensor import SpawnSensor
+from src.test_pkg.scripts.carla_spawn_vehicle import SpawnEgoVehicle
+from src.test_pkg.scripts.object_detection.detect_obstacle import ObjectLocation
+
+
+class GnssData:
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.location = ObjectLocation()
+        rospy.Subscriber('/carla/ego_vehicle/gnss_sensor', NavSatFix, self.callback)
+        rospy.wait_for_message("/carla/ego_vehicle/gnss_sensor", NavSatFix)
+        rospy.spin()
+
+    def callback(self, data: NavSatFix):
+        gnss_const = 0.000009
+        self.y = data.latitude / gnss_const
+        self.x = data.longitude / gnss_const
+        print(self.x, self.y)
+
+        self.location.get_Obstacle_location(self.x, self.y)
+
+    @property
+    def get_gnss_data(self):
+        return self.x, self.y
+
+
+def main():
+    rospy.init_node("av_gnss_subscriber")
+    spawn_vehicle = SpawnEgoVehicle(10, "right")
+    spawn_sensor = SpawnSensor(spawn_vehicle.ego_vehicle_id)
+    gnss_data = GnssData()
+    print(gnss_data.get_gnss_data)
+
+
+if __name__ == "__main__":
+    main()
