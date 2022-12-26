841daa71154357676436f228d99c7b24700dc121
mta <muhammadtahiranis@gmail.com>
2022-12-26 21:13:01 +0500

    v0.0.1

--- src/test_pkg/scripts/object_detection/detect_obstacle.py
+++ src/test_pkg/scripts/object_detection/detect_obstacle.py
@@ -1,13 +1,16 @@
-from src.test_pkg.scripts.path_planning.Shortest_path_with_lanes import LaneList
+
 from src.test_pkg.scripts.path_planning.path_list_maker import path_list
-from src.test_pkg.scripts.path_planning.shortest_path_finder import shortest_path
 from src.test_pkg.scripts.run_ego_vehicle.ego_location import EgoLocation
+from src.test_pkg.scripts.path_planning.Shortest_path_with_lanes import lane_list
+from src.test_pkg.scripts.path_planning.shortest_path_finder import shortest_path
+from src.test_pkg.scripts.object_detection.npc_distance_finder import NpcDistanceFinder
 
 
 
-class ObjectLocation:
+class DetectObstacle:
 
     def __init__(self):
+        self.distance = NpcDistanceFinder()
         pass
 
     @property
@@ -20,23 +23,31 @@ class ObjectLocation:
         end = "3"
         path = route_graph.get_shortest_path_by_road_segments(starting_road_id, end)
 
-        start_lane = "-2"
-        lane_path = LaneList()
+        start_lane = "2"
+        lane_path = lane_list()
         final_path_list = lane_path.lane_in_path(start_lane, path)
+
+        # print(final_path_list)
         return final_path_list
 
-    @classmethod
-    def get_Obstacle_location(cls, x, y):
-        path = ObjectLocation()
+    def get_Obstacle_location(self, x, y):
+        print("x", x,  "y", y)
+        path = DetectObstacle()
         vehicle_path = path.get_path
         location = EgoLocation(x, y)
-        print(location.road_id)
-        print(vehicle_path)
+        obstacle_road_id = location.road_id
+        obstacle_lane_id = location.lane_id
         for road in vehicle_path:
-            if road[0] == location.road_id:
-                print(road[0], location.road_id)
-                if int(road[1]) == int(location.lane_id):
-                    print(road[1], location.lane_id)
+            if road[0] == str(obstacle_road_id):
+                print("road: ", road[0], str(obstacle_road_id))
+            if float(road[1]) == obstacle_lane_id:
+                print("lane: ", road[1], str(location.lane_id))
+        s, t = location.get_ego_location_st
+        print(self.distance.get_unique_st(s, t, obstacle_road_id))
+
+
+
+
 
 
 
